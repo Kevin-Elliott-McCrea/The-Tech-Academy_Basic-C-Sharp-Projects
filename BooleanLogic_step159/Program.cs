4fa8ee401738a6721709bc8d1c4d3a45156731e9
Kevin Elliott McCrea <kleiben@gmail.com>
2020-09-30 16:43:03 -0700

    Add_step159_BooleanLogic

---
+++ BooleanLogic_step159/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic_step159
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int driverAge = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI? (Type Yes or No)");
+            string driverDUI = Console.ReadLine();
+            Console.WriteLine("How many speeding tickets do you have?");
+            int driverTickets = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Are you qualified to be insured?");
+
+            Console.WriteLine(driverAge > 15 && driverDUI == "No" && driverTickets < 4);
+            Console.ReadLine();
+        }
+    }
+}
