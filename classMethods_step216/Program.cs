de709f6cbed7f48026c288454d8eca4df1c75930
Kevin Elliott McCrea <kleiben@gmail.com>
2020-10-09 11:55:20 -0700

    Add_classMethods_step216

---
+++ classMethods_step216/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classMethods_step216
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("What number would you like to be changed? (Pick a whole number that isn't zero)");
+            int userAns = Convert.ToInt32(Console.ReadLine());
+
+            mathClass math1 = new mathClass();
+
+            int ans1 = math1.numAdd(userAns);
+            int ans2 = math1.numTimes(userAns);
+            int ans3 = math1.numDiv(userAns);
+        }
+    }
+}
---
+++ classMethods_step216/mathClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classMethods_step216
+{
+    public class mathClass
+    {
+        public int numAdd(int numToAdd)
+        {
+            numToAdd = numToAdd + 5;
+            Console.WriteLine(numToAdd);
+            return numToAdd;
+        }
+
+        public int numTimes(int numToTimes)
+        {
+            numToTimes = numToTimes * 15;
+            Console.WriteLine(numToTimes);
+            return numToTimes;
+        }
+
+        public int numDiv(int numToDiv)
+        {
+            numToDiv = numToDiv / 2;
+            Console.WriteLine(numToDiv);
+            return numToDiv;
+        }
+
+    }
+}
