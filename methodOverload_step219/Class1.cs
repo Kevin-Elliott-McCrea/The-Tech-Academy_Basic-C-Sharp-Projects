a1c63e541b6ce23a91ec74bf3d5d88cd83c99f80
Kevin Elliott McCrea <kleiben@gmail.com>
2020-10-27 12:48:45 -0700

    Add_methodOverload_step219

---
+++ methodOverload_step219/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classMethods_step216
+{
+    public class mathClass
+    {
+        public int numAdd(int numToAdd)
+        {
+            numToAdd = numToAdd + 5;
+            Console.WriteLine(numToAdd);
+            return numToAdd;
+        }
+
+        public int numAdd(decimal numToTimes)
+        {
+            numToTimes = numToTimes * 15;
+            Console.WriteLine(numToTimes);
+            return Convert.ToInt32(numToTimes);
+        }
+
+        public int numAdd(string numToDiv)
+        {
+            int numToDiv1 = Convert.ToInt32(numToDiv);
+            numToDiv1 = numToDiv1 / 2;
+            Console.WriteLine(numToDiv1);
+            return numToDiv1;
+        }
+
+    }
+}
---
+++ methodOverload_step219/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classMethods_step216
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            mathClass math1 = new mathClass();
+
+            Console.WriteLine("What number would you like to be changed? (Pick a whole number that isn't zero)");
+            int userAns = Convert.ToInt32(Console.ReadLine());
+            int ans1 = math1.numAdd(userAns);
+
+
+            Console.WriteLine("What number would you like to be changed? (Pick any number that isn't zero)");
+            decimal userAns2 = Convert.ToDecimal(Console.ReadLine());
+            int ans2 = math1.numAdd(userAns2);
+
+            // This above function isn't working...
+
+            Console.WriteLine("What number would you like to be changed? (Pick a whole number that isn't zero)");
+            string userAns3 = Console.ReadLine();
+            int ans3 = math1.numAdd(userAns3);
+        }
+    }
+}
