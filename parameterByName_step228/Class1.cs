9f44ab5869ef66c47e8ca28a140704befd4c4259
Kevin Elliott McCrea <kleiben@gmail.com>
2020-12-03 10:40:54 -0800

    Add_step228

---
+++ parameterByName_step228/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step228
+{
+    class SelectParameterByName
+    {
+        public void VoidMethod(int num, int num2)
+        {
+            num = num + 3;
+            Console.WriteLine(num2);
+            Console.WriteLine(num);
+        }
+    }
+}
---
+++ parameterByName_step228/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step228
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SelectParameterByName Instance1 = new SelectParameterByName();
+
+            Instance1.VoidMethod(2, 4);
+            Instance1.VoidMethod(num2: 4, num: 2);
+            Console.ReadLine();
+        }
+    }
+}
