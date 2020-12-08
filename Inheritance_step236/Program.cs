84cbd81954fc79d61fe2523b350d8cee1b5f7880
Kevin Elliott McCrea <kleiben@gmail.com>
2020-12-08 11:45:54 -0800

    Add Inheritance_step236

---
+++ Inheritance_step236/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_step236
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee sampleEmployee = new Employee();
+
+            sampleEmployee.FirstName = "Sample";
+            sampleEmployee.LastName = "Student";
+
+            sampleEmployee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TwentyOne_step208/TwentyOneGame.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne_step208
+{
+    public class TwentyOneGame : Game
+    {
+        public void Play()
+        {
+        }
+    }
+}
