af44e446aa0689f860d744ca4e8bcc0e7dc8c2c4
Kevin Elliott McCrea <kleiben@gmail.com>
2021-01-26 12:46:02 -0800

    Add step252

---
+++ overloadEqualsOperator_step252/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace overloadEqualsOperator_step252
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public static Boolean operator== (Employee Employee1, Employee Employee2)
+        {
+            return Employee1.Id == Employee2.Id;
+        }
+        public static Boolean operator!= (Employee Employee1, Employee Employee2)
+        {
+            return Employee1.Id != Employee2.Id;
+        }
+    }
+}
---
+++ overloadEqualsOperator_step252/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace overloadEqualsOperator_step252
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Emp1 = new Employee
+            {
+                Id = 1
+            };
+            Employee Emp2 = new Employee()
+            {
+                Id = 2
+            };
+
+            if (Emp1 == Emp2)
+            {
+                Console.WriteLine("They are the same employee");
+            }
+            else
+            {
+                Console.WriteLine("They are different");
+            }
+
+            if (Emp1 != Emp2)
+            {
+                Console.WriteLine("They are very different");
+            }
+            else
+            {
+                Console.WriteLine("They are exactly the same");
+            }
+            Console.ReadLine();
+        }
+    }
+}
